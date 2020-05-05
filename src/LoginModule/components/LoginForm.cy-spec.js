a3a85227ecc26adcac381f4e22222ef9ffdbceaf
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-05-05 13:31:44 -0400

    test login and login form

--- src/LoginModule/components/LoginForm.cy-spec.js
+++ src/LoginModule/components/LoginForm.cy-spec.js
@@ -3,46 +3,37 @@ import { mount } from 'cypress-react-unit-test'
 import { LoginForm } from './LoginForm';
 
 describe('LoginForm', () => {
-  test.only('initial state', () => {
+  it('initial state', () => {
     mount(<LoginForm />);
 
-    // it renders empty email and passsword fields
-    const emailField = screen.getByRole('textbox', { name: 'Email' });
-    expect(emailField).toHaveValue('');
-    const passwordField = screen.getByLabelText('Password');
-    expect(passwordField).toHaveValue('');
+    // it renders empty email and password fields
+    cy.get('input#email').should('have.value', '')
+    cy.get('input#password').should('have.value', '')
 
     // it renders enabled submit button
-    const button = screen.getByRole('button');
-    expect(button).not.toBeDisabled();
-    expect(button).toHaveTextContent('Submit');
+    cy.contains('button', 'Submit').should('be.enabled')
   });
 
-  // it('calls onSubmit with form data on submit button click', () => {
-  //   const onSubmitSpy = jest.fn();
-  //   render(<LoginForm onSubmit={onSubmitSpy} />);
+  it('calls onSubmit with form data on submit button click', () => {
+    const onSubmitSpy = cy.stub()
+    mount(<LoginForm onSubmit={onSubmitSpy} />);
 
-  //   const emailField = screen.getByRole('textbox', { name: 'Email' });
-  //   const passwordField = screen.getByLabelText('Password');
-  //   const button = screen.getByRole('button');
+    cy.get('input#email').type('test@email.com')
+    cy.get('input#password').type('password')
+    cy.get('button').click()
+      .then(() => {
+        /* eslint-disable-next-line */
+        expect(onSubmitSpy).to.be.calledWith({
+          email: 'test@email.com',
+          password: 'password'
+        })
+      })
+  });
 
-  //   // fill out and submit form
-  //   fireEvent.change(emailField, { target: { value: 'test@email.com' } });
-  //   fireEvent.change(passwordField, { target: { value: 'password' } });
-  //   fireEvent.click(button);
+  it('updates button on loading state', () => {
+    mount(<LoginForm isLoading />);
 
-  //   expect(onSubmitSpy).toHaveBeenCalledWith({
-  //     email: 'test@email.com',
-  //     password: 'password',
-  //   });
-  // });
-
-  // it('updates button on loading state', () => {
-  //   render(<LoginForm isLoading />);
-
-  //   const button = screen.getByRole('button');
-
-  //   expect(button).toBeDisabled();
-  //   expect(button).toHaveTextContent('Loading...');
-  // });
+    cy.get('button').should('be.disabled')
+      .and('have.text', 'Loading...')
+  });
 });
